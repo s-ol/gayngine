1db5d7f885eb2efc1117d58d259b81dd5c73f09f
s-ol <s-ol@users.noreply.github.com>
2016-07-13 22:19:18 +0200

    lietenant

--- conf.lua
+++ conf.lua
@@ -8,6 +8,7 @@ function love.conf(t)
   t.window.fullscreen = true
   t.window.width = 320 * 6
   t.window.height = 180 * 6
+  t.window.vsync = true
 
   t.modules.joystick = false
   t.modules.physics = false
